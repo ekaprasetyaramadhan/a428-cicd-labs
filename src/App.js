a793b158c76850aafe445dfe821b404c9f827de6
ekaprasetyaramadhan <ekaramadhan35@gmail.com>
2025-01-25 16:17:55 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -8,9 +8,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React! EKAP</h1>
+          <h1 className="App-title">Proyek Membangun CI/CD Pipeline dengan Jenkins</h1>
         </header>
-        <p className="App-intro">Ekaramadhan35</p>
+        <p className="App-intro">
+          Hello, saya eka prasetya ramadhan saat ini sedang belajar implementasi CI CD
+          <br />
+          Applikasi React APP & Simple Python Pyinstaller App
+          <br />
+          CI CD Pipeline menggunakan Jenkins
+          <br />
+          Operation dan Monitoring menggunakan Prometheus dan Grafana
+        </p>
       </div>
     );
   }
